3d606f2bb68c818ee2040b6b7715461795fa5fdf
HirudineeADRT <hirudinee@adroitlogic.com>
2020-04-09 12:32:32 +0530

    change

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.hiru0904Gcp = require("./hiru-0904-gcp.js").handler;
